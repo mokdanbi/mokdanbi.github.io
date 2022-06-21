b5cf5e26506e243239f27531c5d28efdeafc7efb
mokdanbi <l26tempo@naver.com>
2022-06-21 14:10:35 +0900

    20220621

--- trn02/js/0620.js
+++ trn02/js/0620.js
@@ -3,9 +3,15 @@ $(function(){
         $('.topBanner').slideUp();
     });
 
-    $('.mainSlider').on('init afterChange', function(){
+    $('.mainSlider').on('init afterChange', function(e,s,c){
         var current = $('.slick-current');
         current.addClass('on').siblings().removeClass('on');
+        $('.mainArrow i').removeClass('on');
+        if(c === 1) {
+            $('.mainArrow i').addClass('on')
+        };
+
+        $('.slideNum').text()
     });
 
 
@@ -15,5 +21,13 @@ $(function(){
         dots: true,
         pauseOnHover: false,
         pauseOnFocus: false,
+    });
+
+
+    $('.mainArrow i:nth-child(1)').on('click', function(){
+        $('.mainSlider').slick('slickPrev')
+    });
+    $('.mainArrow i:nth-child(2)').on('click', function(){
+        $('.mainSlider').slick('slickNext')
     });
 })
