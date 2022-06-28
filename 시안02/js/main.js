ab3f6c793fd7c98e7917dd004f5e4f66af2a2d75
mokdanbi <l26tempo@naver.com>
2022-06-28 18:31:01 +0900

    0628

--- 시안02/js/main.js
+++ 시안02/js/main.js
@@ -2,6 +2,46 @@ $(function(){
     $('.mainSlider').slick({
         autoplay: true,
         arrows: false,
-        
     });
+
+
+
+    $('.leftSlider').slick({
+        autoplay: true,
+        arrows: false,
+        fade: true,
+        draggable : false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+
+    $('.rightSlider').slick({
+        autoplay: true,
+        arrows: false,
+        draggable : false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        slidesToShow: 3,
+        asNavFor: '.leftSlider'
+    });
+
+    $('.mainSocial .socialArrow i:nth-child(1)').on('click',function(){
+        $('.rightSlider').slick('slickPrev')
+    });
+
+    $('.mainSocial .socialArrow i:nth-child(2)').on('click',function(){
+        $('.rightSlider').slick('slickNext')
+    });
+
+
+
+
+    $('#family').on('change',function(){
+        var link = $(this).val();
+        if(link) {
+            window.open(link);
+        }
+    });
+
+
 })
