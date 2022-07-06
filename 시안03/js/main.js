301d440ab0019ad1389c7386347b2bd2c3abc618
mokdanbi <l26tempo@naver.com>
2022-07-06 10:20:02 +0900

    2022

---
+++ 시안03/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+    $('.main_slider').slick({
+        // autoplay: true,
+        arrows: false,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        autoplaySpeed: 5000,
+        speed: 2000,
+    });
+})
