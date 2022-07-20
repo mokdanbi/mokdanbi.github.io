86591d1ec95b4e1bf41539a53a1ae37e1580ae7e
mokdanbi <l26tempo@naver.com>
2022-07-20 17:45:31 +0900

    0720

--- project05/js/main.js
+++ project05/js/main.js
@@ -47,4 +47,85 @@ $(function(){
         $('.left_tap li').removeClass('on');
         $('.left_tap li').eq(idx).addClass('on');
     });
-})
+
+    
+
+
+    // $('.gnb>ul>li>a').on('click', function (e) {
+    //     if ($('.gnb>ul>li>a ul').hasClass('on')) {
+    //         e.preventDefault();
+    //         // $('.gnb>ul>li>a').next().hide();
+    //         $(this).next().stop().slideToggle();
+    //         $(this).parent().siblings().find('ul').stop().slideUp();
+    //     }
+    // });
+
+    // $('.gnb>ul>li>a').on('click', function () {
+    //     $('.gnb>ul>li>a ul').toggleClass('on');
+    //     $('.gnb>ul>li>a').next().stop().slideUp();
+    // });
+});
+
+
+
+
+
+
+
+
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
+    mapOption = { 
+        center: new kakao.maps.LatLng(35.680343986753506, 127.68768992375809), // 지도의 중심좌표
+        level: 13 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+ 
+// 마커를 표시할 위치와 title 객체 배열입니다 
+var positions = [
+    {
+        title: '부산점', 
+        latlng: new kakao.maps.LatLng(35.15532102489087, 129.06444471394866)
+    },
+    {
+        title: '전주', 
+        latlng: new kakao.maps.LatLng(35.82029813419778, 127.14263040590055)
+    },
+    {
+        title: '울산',
+        latlng: new kakao.maps.LatLng(35.556622562533015, 129.32132391972755)
+    },
+    {
+        title: '제주',
+        latlng: new kakao.maps.LatLng(33.50753937788477, 126.69625029747004)
+    },
+    {
+        title: '대구',
+        latlng: new kakao.maps.LatLng(35.865159785682216, 128.60847328872893)
+    },
+    {
+        title: '서울',
+        latlng: new kakao.maps.LatLng(37.57946608645791, 126.984594423528)
+    }
+];
+
+// 마커 이미지의 이미지 주소입니다
+var imageSrc = "../img/marker.png"; 
+    
+for (var i = 0; i < positions.length; i ++) {
+    
+    // 마커 이미지의 이미지 크기 입니다
+    var imageSize = new kakao.maps.Size(32, 32); 
+    
+    // 마커 이미지를 생성합니다    
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize); 
+    
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        map: map, // 마커를 표시할 지도
+        position: positions[i].latlng, // 마커를 표시할 위치
+        title : positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+        image : markerImage // 마커 이미지 
+    });
+}
