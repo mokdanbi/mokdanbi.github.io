20fe225a3cd489ca9883ab58f61f5ed7cefcea68
mokdanbi <l26tempo@naver.com>
2022-07-28 15:11:33 +0900

    0728

--- trn03/js/main.js
+++ trn03/js/main.js
@@ -5,14 +5,21 @@ $(function(){
     // hide 사라짐
     // slideUp 위로 닫히면서 사라짐
 
-    $('.pop button').on('click', function(){
-        $(this).parent().hide();
-    });
+
+
+
+});
+
+
+
+
+    
+    // $('.pop button').on('click', function(){
+    //     $(this).parent().hide();
+    // });
     // 팝업창 하나씩 닫힘
 
     // $('.pop button').on('click', function(){
     //     $('.pop').hide();
     // });
     // 팝업창 한꺼번에 닫힘
-
-});
