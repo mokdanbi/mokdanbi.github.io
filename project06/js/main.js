1072e98f57e76ccbaa7e26d77aea968103985ebf
mokdanbi <l26tempo@naver.com>
2022-08-04 17:50:51 +0900

    0804

--- project06/js/main.js
+++ project06/js/main.js
@@ -3,11 +3,28 @@ $(function () {
         $('.top_banner').slideUp();
     });
 
+
+
     $('.main_slider').slick({
         arrows: false,
         slidesToShow: 1,
         centerMode: true,
         variableWidth: true,
+    });
 
+
+
+    $('.mall_slider').slick({
+        arrows: false,
+        slidesToShow: 5,
+        centerMode: true,
+    });
+
+    $('.mall_arrows i:nth-child(1)').on('click', function(){
+        $('.mall_slider').slick('slickPrev')
+    });
+
+    $('.mall_arrows i:nth-child(2)').on('click', function(){
+        $('.mall_slider').slick('slickNext')
     });
 });
