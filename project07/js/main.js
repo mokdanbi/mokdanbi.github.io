eb07cf116b610975b4b1492c595b86d8903648bc
mokdanbi <l26tempo@naver.com>
2022-08-10 17:49:40 +0900

    0810

---
+++ project07/js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('.MainSlider').slick({
+        arrows: false,
+    });
+
+    $('.TxtSlide').slick({
+        arrows: false,
+        fade: true,
+        // slidesToShow: 1,
+    });
+
+    $('.BestImg').slick({
+        arrows: false,
+        slidesToShow: 3,
+        // centerMode: true,
+    });
+})
