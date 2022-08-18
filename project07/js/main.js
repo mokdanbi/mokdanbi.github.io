cad536a349a6436a3448c3bb3730c175233a7e92
mokdanbi <l26tempo@naver.com>
2022-08-18 17:49:16 +0900

    0818

--- project07/js/main.js
+++ project07/js/main.js
@@ -1,6 +1,11 @@
 $(function () {
+
     $('.MainSlider').slick({
         arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
     });
 
     $('.TxtSlide').slick({
@@ -31,9 +36,65 @@ $(function () {
 
 
 
-    
-    $(".MainBrand>.brandTap>ul>li").click(function(){
+
+    $(".MainBrand>.brandTap>ul>li").click(function () {
         $(".MainBrand>.brandTap>ul>li").removeClass("on");
         $(this).addClass("on");
     });
-})
+
+
+    const S = $('.MainInsta .slider').slick({
+        slidesToShow: 5,
+        arrows: false,
+    });
+
+    $('.MainInsta').on('mousemove', e => {
+        e.pageX > $(this).width() / 2
+            ? S.slick('slickPrev')
+            : S.slick('slickNext')
+    });
+
+
+    $('.searchclick').on('click', function(e){
+        e.preventDefault();
+        $('.searchBox').show('on');
+    })
+
+    
+    $('.searchClose').on('click', function(e){
+        e.preventDefault();
+        $('.searchBox').hide('on');
+    })
+
+
+    $('.scrollBtn i:nth-child(1)').on('click', function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+    $('.scrollBtn i:nth-child(2)').on('click', function () {
+        $('html,body').animate({
+            scrollTop: 5000
+        }, 600);
+        return false;
+    });
+
+});
+
+
+function searchAction() {
+    var searchValue = $('.searchForm input').val();
+    // console.log(searchValue)
+    if (searchValue.length < 2) {
+        alert('2글자 이상 입력해주세요.')
+    } else {
+        alert(searchValue)
+    }
+
+    $('.searchForm input').val('');
+    // 검색 후에 초기화
+    $('.searchForm input').focus();
+    // 검색 후에도 계속 입력상태로 유지
+};
