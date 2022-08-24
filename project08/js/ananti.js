fe32ff7c10f2355fd436c62f4a41704861ceee9b
mokdanbi <l26tempo@naver.com>
2022-08-24 17:52:57 +0900

    0824

--- project08/js/ananti.js
+++ project08/js/ananti.js
@@ -2,13 +2,32 @@ window.addEventListener('DOMContentLoaded', () => {
     const mainSlider = new Swiper('.mainSlider', {
         loop: true,
         autoplay: {
-            delay: 2500,
+            delay: 4000,
             disableOnInteraction: false,
         },
-        // pagination: {
-        //     el: ".swiper-pagination",
-        //     clickable: true,
-        // },
+    });
+
+    const aboutSlider = new Swiper('.aboutSlider', {
+        effect: 'fade',
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+    });
+
+    const roomSlider = new Swiper('.roomSlider', {
+        // spaceBetween: 30,
+        centeredSlides: true,
+        slidesPerView: 3,
+        loop: true,
+        pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
     });
 
 
