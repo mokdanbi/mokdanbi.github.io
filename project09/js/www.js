3f40a4bf90272f4feb049794157b62b3ccce4882
mokdanbi <l26tempo@naver.com>
2022-08-31 17:49:52 +0900

    0831

---
+++ project09/js/www.js
@@ -0,0 +1,29 @@
+window.addEventListener('DOMContentLoaded', () => {
+    function bindingAccordionEvent(wrap) {
+        let areaArr = document.querySelectorAll(wrap);
+
+        areaArr.forEach(function (area) {
+            let hasGroup = area.dataset['group'] !== undefined ? true : false;
+            let btn = area.querySelector('.btn_toggle');
+
+            btn.addEventListener('click', function () {
+                if (hasGroup === true) {
+                    let name = area.dataset['group'];
+                    let groupArr = document.querySelectorAll(wrap + '[data-group="' + name + '"]');
+                    let thisContent = area.querySelector('.content_area');
+
+                    groupArr.forEach(function (group) {
+                        let content = group.querySelector('.content_area');
+                        content.classList.remove('act');
+                    });
+                    thisContent.classList.add('act');
+                } else {
+                    let content = area.querySelector('.content_area');
+                    content.classList.toggle('act');
+                }
+            });
+        });
+    }
+
+    bindingAccordionEvent('.accordion_area');
+});
