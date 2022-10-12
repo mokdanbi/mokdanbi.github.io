9c6869ad4d6aad3db503b941cb0f1e369f50fc03
mokdanbi <l26tempo@naver.com>
2022-10-12 17:51:31 +0900

    1012

--- project03/js/main.js
+++ project03/js/main.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     $('.main_slider').slick({
         autoplay: true,
         arrows: false,
@@ -15,29 +15,29 @@ $(function() {
         arrows: false,
         pauseOnHover: false,
         pauseOnFocus: false,
-        draggable : false,
+        draggable: false,
         slidesToShow: 2,
     });
-    
+
     $('.menu_bottom').slick({
         autoplay: true,
         arrows: false,
         pauseOnHover: false,
         pauseOnFocus: false,
-        draggable : false,
+        draggable: false,
         slidesToShow: 2,
         asNavFor: '.menu_top'
     });
 
 
-    $('.main_menu .menu_arrows i:nth-child(1)').on('click', function(){
+    $('.main_menu .menu_arrows i:nth-child(1)').on('click', function () {
         $('.menu_bottom').slick('slickPrev')
     });
 
-    $('.main_menu .menu_arrows i:nth-child(2)').on('click', function(){
+    $('.main_menu .menu_arrows i:nth-child(2)').on('click', function () {
         $('.menu_bottom').slick('slickNext')
     });
-    
+
 
     $('.vegan_slider').slick({
         autoplay: true,
@@ -50,11 +50,20 @@ $(function() {
     });
 
 
-    $('.to_top').on('click', function (){
-        $('html,body').animate({scrollTop:0}, 300)  // 기본 400
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll > 3700) {
+            $(".to_top i").css("color", "#ffffff");
+        } else {
+            $(".to_top i").css("color", "");
+        }
     });
 
-    $(window).on('scroll',function(){
+    $('.to_top').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 300)  // 기본 400
+    });
+
+    $(window).on('scroll', function () {
         var sct = $(window).scrollTop();
         if (sct > 500) {
             $('.to_top').fadeIn(500)
@@ -62,5 +71,4 @@ $(function() {
             $('.to_top').fadeOut(500)
         }
     });
-
 })
