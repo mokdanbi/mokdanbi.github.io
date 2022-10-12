a9136d8125e3c28fd2a706cee914f234c609b721
mokdanbi <l26tempo@naver.com>
2022-10-12 14:32:41 +0900

    1012

--- project02/js/main.js
+++ project02/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
 
     $('.mainSlider').slick({
@@ -9,18 +9,18 @@ $(function(){
         pauseOnFocus: false,
     });
 
-    
+
 
     $('.newsSlider').slick({
         arrows: false,
         slidesToShow: 3,
     });
 
-    $('.mainNews .newsArrow i:nth-child(1)').on('click',function(){
+    $('.mainNews .newsArrow i:nth-child(1)').on('click', function () {
         $('.newsSlider').slick('slickPrev')
     });
 
-    $('.mainNews .newsArrow i:nth-child(2)').on('click',function(){
+    $('.mainNews .newsArrow i:nth-child(2)').on('click', function () {
         $('.newsSlider').slick('slickNext')
     });
 
@@ -30,7 +30,7 @@ $(function(){
         autoplay: true,
         arrows: false,
         fade: true,
-        draggable : false,
+        draggable: false,
         pauseOnHover: false,
         pauseOnFocus: false,
     });
@@ -38,18 +38,18 @@ $(function(){
     $('.rightSlider').slick({
         autoplay: true,
         arrows: false,
-        draggable : false,
+        draggable: false,
         pauseOnHover: false,
         pauseOnFocus: false,
         slidesToShow: 3,
         asNavFor: '.leftSlider'
     });
 
-    $('.mainSocial .socialArrow i:nth-child(1)').on('click',function(){
+    $('.mainSocial .socialArrow i:nth-child(1)').on('click', function () {
         $('.rightSlider').slick('slickPrev')
     });
 
-    $('.mainSocial .socialArrow i:nth-child(2)').on('click',function(){
+    $('.mainSocial .socialArrow i:nth-child(2)').on('click', function () {
         $('.rightSlider').slick('slickNext')
     });
 
@@ -57,28 +57,84 @@ $(function(){
     $('.titleSlider').slick({
         autoplay: true,
         arrows: false,
-        draggable : false,
+        draggable: false,
         pauseOnHover: false,
         pauseOnFocus: false,
         fade: true,
         slidesToShow: 1,
     });
 
-    $('.mainSocial .socialArrow i:nth-child(1)').on('click',function(){
+    $('.mainSocial .socialArrow i:nth-child(1)').on('click', function () {
         $('.titleSlider').slick('slickPrev')
     });
 
-    $('.mainSocial .socialArrow i:nth-child(2)').on('click',function(){
+    $('.mainSocial .socialArrow i:nth-child(2)').on('click', function () {
         $('.titleSlider').slick('slickNext')
     });
 
 
 
-    $('#family').on('change',function(){
+    $('#family').on('change', function () {
         var link = $(this).val();
-        if(link) {
+        if (link) {
             window.open(link);
         }
+    });
+
+
+    
+
+
+    
+    $('.mainBusiness .bnLeft_left figure:nth-child(1)').hover(function () {
+        $('.mainBusiness .bnRight ul>li:nth-child(1)>i').css({ display: 'block' });
+    }, function() {
+        $('.mainBusiness .bnRight ul>li:nth-child(1)>i').css({ display: 'none' });
+    });
+
+    $('.mainBusiness .bnRight ul>li:nth-child(1)').hover(function () {
+        $('.mainBusiness .bnRight ul>li:nth-child(1)>i').css({ display: 'block' });
+    }, function() {
+        $('.mainBusiness .bnRight ul>li:nth-child(1)>i').css({ display: 'none' });
+    });
+
+    
+    $('.mainBusiness .bnLeft_right figure:nth-child(1)').hover(function () {
+        $('.mainBusiness .bnRight ul>li:nth-child(2)>i').css({ display: 'block' });
+    }, function() {
+        $('.mainBusiness .bnRight ul>li:nth-child(2)>i').css({ display: 'none' });
+    });
+
+    $('.mainBusiness .bnRight ul>li:nth-child(2)').hover(function () {
+        $('.mainBusiness .bnRight ul>li:nth-child(2)>i').css({ display: 'block' });
+    }, function() {
+        $('.mainBusiness .bnRight ul>li:nth-child(2)>i').css({ display: 'none' });
+    });
+
+    
+    $('.mainBusiness .bnLeft_left figure:nth-child(2)').hover(function () {
+        $('.mainBusiness .bnRight ul>li:nth-child(3)>i').css({ display: 'block' });
+    }, function() {
+        $('.mainBusiness .bnRight ul>li:nth-child(3)>i').css({ display: 'none' });
+    });
+
+    $('.mainBusiness .bnRight ul>li:nth-child(3)').hover(function () {
+        $('.mainBusiness .bnRight ul>li:nth-child(3)>i').css({ display: 'block' });
+    }, function() {
+        $('.mainBusiness .bnRight ul>li:nth-child(3)>i').css({ display: 'none' });
+    });
+
+    
+    $('.mainBusiness .bnLeft_right figure:nth-child(2)').hover(function () {
+        $('.mainBusiness .bnRight ul>li:nth-child(4)>i').css({ display: 'block' });
+    }, function() {
+        $('.mainBusiness .bnRight ul>li:nth-child(4)>i').css({ display: 'none' });
+    });
+
+    $('.mainBusiness .bnRight ul>li:nth-child(4)').hover(function () {
+        $('.mainBusiness .bnRight ul>li:nth-child(4)>i').css({ display: 'block' });
+    }, function() {
+        $('.mainBusiness .bnRight ul>li:nth-child(4)>i').css({ display: 'none' });
     });
 
 
